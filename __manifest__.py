1a6bbf08d7744b17d6b557c77b105fe6f6264827
Martin Allimonier <allimonier.martin@gmail.com>
2018-02-01 11:38:08 +0100

    Initial Commit

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Custom Zcaisse",
+    'name': "Price label",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        """,
 
     'description': """
-        Long description of module's purpose
+        
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Noosys",
+    'website': "",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
